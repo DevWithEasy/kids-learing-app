ce32b00ca55a5325a6b79ea8b06fafa26b353a87
DevWithEasy <devwitheasy@gmail.com>
2024-05-22 16:59:07 +0600

    audio play add

--- utils/playAudio.js
+++ utils/playAudio.js
@@ -1,6 +1,8 @@
-const playAudio=(url)=>{
-    const audio = new Audio(url)
-    audio.play()
+import { Audio } from 'expo-av'
+
+const playAudio = async (url) => {
+    const { sound } = await Audio.Sound.createAsync(url)
+    await sound.playAsync()
 }
 
 export default playAudio
